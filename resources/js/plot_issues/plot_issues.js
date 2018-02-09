d7bc8707ff3f5856c778f5c68f27f27d2b09c538
niordsid <acsantanab@outlook.com>
2018-02-09 15:52:49 -0500

    Change logic for show the correct data (sessions, issues) on the input_fields according to the student selected

--- resources/js/plot_issues/plot_issues.js
+++ resources/js/plot_issues/plot_issues.js
@@ -4,6 +4,7 @@
 
 var students = [];
 var sessions = [];
+var unique_array = [];
 var issues = [];
 var data = [];
 
@@ -16,16 +17,12 @@ var charta = null;
 
 var initialize = function() {
   $('#session').prop('disabled', true);
+  $('#issues-list').prop('disabled', true);
 
 
   getListStudents(function(_students) {
     students = _students;
     renderStudents();
-  });
-
-  getListIssues(function(_issues) {
-    issues = _issues;
-    renderIssues();
   });
 
 };
@@ -58,18 +55,6 @@ var getListSessions = function(student_id, callback) {
     });
 };
 
-var getListIssues = function(callback) {
-  $.ajax({
-      url: "https://api.arca.acacia.red/list/Issue",
-      method: 'GET',
-      dataType: "json"
-    })
-    .done(callback)
-    .fail(function(error) {
-      console.error('Error', error);
-    });
-};
-
 var getStudentsIssues = function(student_id, callback) {
   $.ajax({
       url: "https://api.arca.acacia.red/plot/issue_student",
@@ -94,27 +79,43 @@ var renderStudents = function() {
   $('#student').append(html);
 };
 
-var renderSession = function() {
-  let html = '<option value="">Choose Session</option>';
-  for (let i = 0; i < sessions.length; i++) {
-    html += '<option value="' + sessions[i]['Session'] + '">' + sessions[i]['Session'] + "</option>";
+var renderSession = function(data) {
+  let html = "";
+  sessions = [];
+  unique_array = [];
+  let data_student = Object.keys(data);
+  for (let i = 0; i < data_student.length; i++) {
+    let data_filter = data[data_student[i]];
+    Object.keys(data_filter).forEach(function(key) {
+      let sessions_object = data_filter[key];
+      let sess_array = Object.keys(sessions_object);
+      for (let i = 0; i < sess_array.length; i++) {
+        sessions.push(sess_array[i]);
+        for (let i = 0; i < sessions.length; i++) {
+          if (unique_array.indexOf(sessions[i]) == -1) {
+            unique_array.push(sessions[i])
+          }
+        }
+      }
+    });
   }
-  $('#session').html(html);
+
+  for (i = 0; i < unique_array.length; i++) {
+    html += '<option value="' + unique_array[i] + '">' + unique_array[i] + "</option>";
+  }
+  $('#session').append(html);
 };
 
-var renderIssues = function() {
+var renderIssues = function(data) {
   let html = "";
-  for (let i = 0; i < issues.length; i++) {
-    html += '<option value="' + issues[i]['Issue'] + '">' + issues[i]['Issue'] + "</option>";
+  let issue_list = Object.keys(data);
+  for (let i = 0; i < issue_list.length; i++) {
+    html += '<option value="' + issue_list[i] + '">' + issue_list[i] + "</option>";
   }
   $('#issues-list').append(html);
 };
 
-
-
 var generateChar = function(name_series, data_series) {
-  // console.log("series", name_series);
-  // console.log("datos=", data_series);
   charta = new Highcharts.chart('container', {
     chart: {
       type: 'column',
@@ -193,30 +194,14 @@ var findbyKey = function(issu, filt, delt) {
   generateChar(name_issue, seri);
 
 }
+
 var plotGraphic = function() {
-  getStudentsIssues(selected_student, function(_data) {
-    data = _data;
-    if ($('#manually').is(':checked')) {
-      let filter = "Human_Observation";
-      let remove = "Digital_Observation";
-      findbyKey(data, filter, remove);
-    } else if ($('#automatically').is(':checked')) {
-      let filter = "Digital_Observation";
-      let remove = "Human_Observation";
-      findbyKey(data, filter, remove);
-    } else {
-      console.log("You should select a kind of filter");
-    }
-
-  });
-
-
   if (charta) {
     charta.destroy();
   }
-
-
 };
+
+
 //------------------------------------------------------
 //  DOCUMENT READY
 //------------------------------------------------------
@@ -226,37 +211,51 @@ $(document).ready(function() {
   //  EVENTS
   //------------------------------------------------------
 
-  $('.check-input').on('change', function() {
-    $('.check-input').not(this).prop('checked', false);
-  });
+
 
   $('#student').change(function() {
     selected_student = $(this).val() !== "" ? $(this).val() : null;
-
-
-    $('#session').prop('disabled', true);
-    $('#session').html('<option value="">Choose Session</option>');
+    $('#session').prop('disabled', true); // Disabled sessions list when the user change the Student Selected
+    $('#issues-list').prop('disabled', true); // Disabled isues list when the user change the Student Selected
     if (selected_student) {
-      getListSessions(selected_student, function(_sessions) {
+      getStudentsIssues(selected_student, function(_data) {
+        data = _data;
+        $('#session').prop('disabled', false);
+        $('#session').html('<option value="">Choose Session</option>');
+        $('#issues-list').prop('disabled', false);
+        $('#issues-list').html('<option value="">Choose Issue</option>');
+        if ($("#manually").is(':checked')) {
+          renderIssues(data);
+          renderSession(data);
+        } else if ($("#automatically").is(':checked')) {
+          renderIssues(data);
+          renderSession(data);
+        } else {
 
-        if (_sessions.length) {
-          sessions = _sessions;
-          $('#session').prop('disabled', false);
-          renderSession();
         }
 
 
+
+        // if ($('#manually').is(':checked')) {
+        //   let filter = "Human_Observation";
+        //   let remove = "Digital_Observation";
+        //   findbyKey(data, filter, remove);
+        // } else if ($('#automatically').is(':checked')) {
+        //   let filter = "Digital_Observation";
+        //   let remove = "Human_Observation";
+        //   findbyKey(data, filter, remove);
+        // } else {
+        //   console.log("You should select a kind of filter");
+        // }
+        // if (_sessions.length) {
+        //   sessions = _sessions;
+        //   $('#session').prop('disabled', false);
+        //   renderSession();
+        // }
       });
-
     }
-
-
   });
 
-  $('#session').change(function() {
-    selected_session = $(this).val() !== "" ? $(this).val() : null;
-
-  });
 
 
 
